759a6b03986814cd95c5ab94f3c8aab262c75c57
coolistkovbel <coolistkovbel@gmail.com>
2023-03-24 16:03:25 -0400

    eh

--- README.md
+++ README.md
@@ -2,31 +2,29 @@
 
 Current Testing Site URL: https://de-cyber-hub.vercel.app/
 
-This will be a small community nft projects. Where Basically you will be able to trade your NFT for another. OR sell it on the market. 
-Eventually implement that bitcoin nft and maybe put on nervos network. to make it avaible for all blockchains. HMMMMMMMM
-
 ## Technology Stack & Tools
 
-- Solidity (Writing Smart Contracts & Tests)
-- Javascript (React & Testing)
+- [Solidity](https://docs.soliditylang.org/en/latest/)(Writing Smart Contracts & Tests)
+- [Javascript](https://javascript.info/)(React & Testing)
 - [Hardhat](https://hardhat.org/) (Development Framework)
 - [Ethers.js](https://docs.ethers.io/v5/) (Blockchain Interaction)
 - [React.js](https://reactjs.org/) (Frontend Framework)
 - [Socket.io](https://socket.io/) (Client & Server communication)
 
 ## Requirements For Initial Setup
+
+**be sure to use node -v 14.17.3**
+
 - Install [NodeJS](https://nodejs.org/en/)
 
 ## Setting Up
+
 ### 1. Clone/Download the Repository
 
 ### 2. Install Dependencies:
-**be sure to use node -v 14.17.3**
+
 `$ npm install`
 
-### 3. 
+### 3. If you are going to be doing requests be sure to describe the changes created. Use the Issue tab for an questions
 
 SHIB ETH Contract: 0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce
-
-
-Want to also do a nice little credit section. There are a lot a lot a lot of people that i would like to say helped me out and continue to help me out and pursue coding and make it fun and easy. Just need you guys to create a better community right now. if you are down... let me know #deBlockchainDev
--- src/App.js
+++ src/App.js
@@ -8,128 +8,121 @@ import EnterHub from "./EnterHub";
 // ABIs
 import contractAbi from "./abis/deHub.json";
 
-
 // Contract Address here
-const contractAddress = '0xb55aafcC47057C62a717D1b5d709d3a21884227C'
-
+const contractAddress = "0xb55aafcC47057C62a717D1b5d709d3a21884227C";
 
 function App() {
-
-  const [currentAccount, setCurrentAccount] = useState([])
-  const [currentAmountOfAccount, setCurrentAmountOfAccount] = useState("")
-
+  const [currentAccount, setCurrentAccount] = useState([]);
+  const [currentAmountOfAccount, setCurrentAmountOfAccount] = useState("");
 
   // We are first going to grab the proivders and signers
   const getEthereum = () => {
-    const {ethereum} = window
-    const provider = new ethers.providers.Web3Provider(ethereum)
-    const signer = provider.getSigner()
-    const deHubContract = new ethers.Contract(contractAddress, contractAbi, signer)
-    return deHubContract
-  }
+    const { ethereum } = window;
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const deHubContract = new ethers.Contract(
+      contractAddress,
+      contractAbi,
+      signer
+    );
+    return deHubContract;
+  };
 
   // Check to see if our metamask is connected to our website
   const checkIfWalletIsConnected = async () => {
     try {
-      
-      const {ethereum} = window;
+      const { ethereum } = window;
 
-      if(!ethereum) {
+      if (!ethereum) {
         alert("Must have metamask installed...");
         return;
-      } else{
+      } else {
         console.log("We have a ethereum object");
       }
-
 
       // Check to see if we have authorization to users wallet...
       const accounts = await ethereum.request({ method: "eth_accounts" });
 
-      if(accounts.length > 0) {
-        const account = accounts[0]
-        console.log('There is an account: ', account)
-        setCurrentAccount(account)
-      } else{
-        console.log('You failed bro')
+      if (accounts.length > 0) {
+        const account = accounts[0];
+        console.log("There is an account: ", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("You failed bro");
       }
 
       // Access smart contract and set the total number of blockchain users in deCyberHub smart contract
-      const deHubContract = getEthereum()
-      let count = await deHubContract.getTotalAccounts()
-      setCurrentAmountOfAccount(count.toNumber())
-
+      const deHubContract = getEthereum();
+      let count = await deHubContract.getTotalAccounts();
+      setCurrentAmountOfAccount(count.toNumber());
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-
-  }
+  };
 
   // Creating a method for users to connect to smart contract if not already connected
   const connectWallet = async () => {
-    try{
-      const {ethereum} = window
-      
-      if(!ethereum){
-        alert('Y U NO HAVE METAMASK ')
-        return
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Y U NO HAVE METAMASK ");
+        return;
       }
 
       // Going to need to setup to grab all the accounts again.. maybe
-
-    }catch(error){
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
-  }
+  };
 
   const createAccount = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-
       // We are calling the smart contract and putting it into a variable
       const deHubContract = getEthereum();
       // We run the create account function from smart contract
-      let deAccount = await deHubContract.createAccount(e.target.elements[0].value, e.target.elements[1].value)
+      let deAccount = await deHubContract.createAccount(
+        e.target.elements[0].value,
+        e.target.elements[1].value
+      );
       // We look for transaction hash
-      console.log('mining trx', deAccount.hash)
+      console.log("mining trx", deAccount.hash);
       // We wait till the transaction is finished
-      await deAccount.wait()
+      await deAccount.wait();
 
-      let count = await deHubContract.getTotalAccounts()
-      setCurrentAmountOfAccount(count.toNumber())
+      let count = await deHubContract.getTotalAccounts();
+      setCurrentAmountOfAccount(count.toNumber());
 
       // We reset the values
-      e.target.elements[0].value = ""
-      e.target.elements[1].value = ""
-
-
+      e.target.elements[0].value = "";
+      e.target.elements[1].value = "";
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-
-
-  }
+  };
 
   useEffect(() => {
-    checkIfWalletIsConnected()
-  }, [])
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
     <div className="App">
-      <Header 
-        account={currentAccount} 
-        setAccount={setCurrentAccount} 
-        connectWallet={connectWallet} />
-      <EnterHub 
-        createAccount={createAccount} 
+      <Header
+        account={currentAccount}
+        setAccount={setCurrentAccount}
+        connectWallet={connectWallet}
+      />
+      <EnterHub
+        createAccount={createAccount}
         currentAmountOfAccounts={currentAmountOfAccount}
-        />  
+      />
     </div>
   );
 }
 
 export default App;
-
 
 // import { io } from "socket.io-client";
 
